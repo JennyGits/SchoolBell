ffe20ce3f443826abf6d5dd8c26382cf60b00e29
WON-coder <ic626847@gmail.com>
2020-11-03 12:46:13 +0900

    commit

--- app/src/main/java/com/example/schoolbell/ModifyTimetableActivity.java
+++ app/src/main/java/com/example/schoolbell/ModifyTimetableActivity.java
@@ -1,9 +1,15 @@
 package com.example.schoolbell;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ModifyTimetableActivity extends AppCompatActivity {
+
+    ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
