0597f42551997759d2012cd5903a4088da809b15
WON-coder <ic626847@gmail.com>
2020-11-03 12:34:53 +0900

    First timetable

---
+++ app/src/main/java/com/example/schoolbell/ModifyTimetableActivity.java
@@ -0,0 +1,13 @@
+package com.example.schoolbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyTimetableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_timetable);
+    }
+}
