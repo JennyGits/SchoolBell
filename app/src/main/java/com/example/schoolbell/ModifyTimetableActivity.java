bf8ae3e9cd94ba3a721057391f31a3d1b5145784
WON-y <ic626847@gmail.com>
2020-11-24 12:12:24 +0900

    modify timetable and save data

--- app/src/main/java/com/example/schoolbell/ModifyTimetableActivity.java
+++ app/src/main/java/com/example/schoolbell/ModifyTimetableActivity.java
@@ -2,13 +2,17 @@ package com.example.schoolbell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -61,10 +65,26 @@ public class ModifyTimetableActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getApplicationContext(),timetableActivity.class);
                 intent.putExtra("textList", list);
-                startActivity(intent);
-
-
+                setStringArrayPref(ModifyTimetableActivity.this,"jenny",list);
+                //startActivity(intent);
+                setResult(1001,intent);
+                finish();
             }
         });
+    }
+
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/schoolbell/timetableActivity.java
+++ app/src/main/java/com/example/schoolbell/timetableActivity.java
@@ -1,32 +1,45 @@
 package com.example.schoolbell;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+
 
 public class timetableActivity extends AppCompatActivity {
 
     //final TextView output = (TextView) findViewById(R.id.text_output);
 
+    private SharedPreferences mPreferences;
+    private String SharedPrefFile = "com.example.schoolbell";
 
+
+    //ArrayList<String> list;
     ArrayList<String> list;
     TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
 
-        list = getIntent().getStringArrayListExtra("textList");
+
+        list = getStringArrayPref(this,"jenny");
         if(list == null) list = new ArrayList<>();
         int p=0;
 
@@ -38,13 +51,21 @@ public class timetableActivity extends AppCompatActivity {
                 text = findViewById(id);
                 try {
                     text.setText(list.get(p));
-                    Log.i("string", list.get(p));
                 }catch(Exception e){
                     text.setText("");
                 }
                 p++;
             }
         }
+
+        Button gobackBtn = findViewById(R.id.gobackBtn);
+        gobackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
@@ -73,8 +94,55 @@ public class timetableActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getApplicationContext(),ModifyTimetableActivity.class);
                 intent.putExtra("textList", list);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        list = data.getStringArrayListExtra("textList");
+        if(list == null) list = new ArrayList<>();
+        int p=0;
+
+        for(int i=0;i<7;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                int id = getResources().getIdentifier("table_"+i+"_"+j, "id", "com.example.schoolbell");
+                text = findViewById(id);
+                try {
+                    text.setText(list.get(p));
+                }catch(Exception e){
+                    text.setText("");
+                }
+                p++;
+            }
+        }
+
+    }
+
+
+
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 }
